007abe0a5ce5f489d8babd75409e0d3e34d0b2dc
Matt Hess <matt.hess@lookoutmountainresearch.com>
2018-06-15 22:57:30 -0400

    Removed if statement from the image.

--- www/index.php
+++ www/index.php
@@ -1,6 +1,4 @@
 <?php
-    $result = filter_input ( INPUT_GET , 'image' );
-    if (isset($result) && !empty($result)) {
-        echo '<img src="images\LookoutMountainResearch_Logo.png">';
-    }
+          echo '<img src="images\LookoutMountainResearch_Logo.png">';
+
 ?>
