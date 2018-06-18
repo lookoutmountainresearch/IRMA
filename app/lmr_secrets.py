36fbe38961465284dbb3d874d8a2243e68b9e362
Matt Hess <matt.hess@lookoutmountainresearch.com>
2018-06-17 21:38:07 -0400

    Added Database script & tests.

---
+++ app/lmr_secrets.py
@@ -0,0 +1,8 @@
+def load_db_secrets():
+    db_secrets = {
+        "db_hostname": "52.14.44.108",
+        "db_instance": "lookoutmountainresearch_main_test",
+        "db_user": "lmr_dm_testuser",
+        "db_password": "N0th1ng#01"
+    }
+    return db_secrets
